f4e0fd1e19009ee6b0f87f7eddc05d0d228ffc4a
Chris Kronberg <ckronber@gmail.com>
2022-02-07 18:30:41 -0500

    updated code to have a cache

--- blogPosts/controllers/getPosts.py
+++ blogPosts/controllers/getPosts.py
@@ -1,17 +1,17 @@
-from distutils.log import error
-from flask_restful import Api, Resource, abort, fields
-from models.posts import Post,user_posts
-from flask import jsonify
+from flask_restful import Resource
+from models.posts import user_posts
+from flask import jsonify,json
 import json
 import requests
+import asyncio
 
 class postRequests(Resource):
-    tag_error = {"error":"tag parameter is required"}
-    sort_error = {"error":"sortBy paremeter is invalid"}
-    dir_error = {"error":"direction parameter is invalid"}
-
     def __init__(self):
-        pass
+        self.tag_error = {"error":"tag parameter is required"}
+        self.sort_error = {"error":"sortBy paremeter is invalid"}
+        self.dir_error = {"error":"direction parameter is invalid"}
+        self.tag_store = []
+        self.cache_storage = {}
 
     def sortId(self,e):
         return e['id']
@@ -25,44 +25,63 @@ class postRequests(Resource):
     def deserialize(self,myInput):
         schema = user_posts(many=True)
         result = schema.load(myInput)
-        #posts = []
-        #for p in myPost["posts"]:
-        #    result = schema.load(p)
-        #    posts.append(result)
-        return result
+        return json(result)
+
+    def serialize(self,input_posts):    
+        mySchema = user_posts(many=True)
+        j_result = mySchema.dump(input_posts)
+        return j_result
+
+    def cache_store(self,tag):
+        if(tag in self.tag_store):
+            print("brought from cache")
+            return self.cache_storage[tag]
+        else:
+            print("stored to cache")
+            self.tag_store.append(tag)
+            get_data = requests.get('https://api.hatchways.io/assessment/blog/posts',params={"tag":tag})
+            json_data = get_data.json()
+            self.cache_storage[tag] = json_data
+            return json_data
 
     def get(self,tag:str,sortBy:str = None,direction:str = None):
         if(tag):
-            get_data = requests.get('https://api.hatchways.io/assessment/blog/posts',params={"tag":tag})
-            result = get_data.json()
+            #get_data = requests.get('https://api.hatchways.io/assessment/blog/posts',params={"tag":tag})
+            get_data = self.cache_store(tag)
+            #result = get_data.json()
+            result = get_data
             if(sortBy):
                 if(sortBy == "id"):
                     if(direction == "desc"):
                         result["posts"].sort(key=self.sortId,reverse=True)
-                    elif(direction == "asc" or direction ==None):
+                    elif(direction == "asc" or direction == None):
                         result["posts"].sort(key=self.sortId)
                     else:
-                        return self.dir_error,400
+                        return jsonify(self.dir_error),400
                 elif(sortBy == "reads"):
                     if(direction == "desc"):
                         result["posts"].sort(key=self.sortReads,reverse=True)
-                    elif(direction == "asc" or direction ==None):
+                    elif(direction == "asc" or direction == None):
                         result["posts"].sort(key=self.sortReads)
                     else:
-                        return self.dir_error,400
+                        return jsonify(self.dir_error),400
                 elif(sortBy == "likes"):
                     if(direction == "desc"):
                         result["posts"].sort(key=self.sortLikes,reverse=True)
-                    elif(direction == "asc" or direction ==None):
+                    elif(direction == "asc" or direction == None):
                         result["posts"].sort(key=self.sortLikes)
+                    else:
+                        return jsonify(self.dir_error),400
                 elif(sortBy == "popularity"):
                     if(direction == "desc" ):
                         result["posts"].sort(key=self.sortPop,reverse=True)
-                    elif(direction == "asc" or direction ==None):
+                    elif(direction == "asc" or direction == None):
                         result["posts"].sort(key=self.sortPop)
+                    else:
+                        return jsonify(self.dir_error),400
                 else:
-                    return self.sort_error,400
+                    return jsonify(self.sort_error),400
         else:
-           return self.tag_error,400
+           return jsonify(self.tag_error),400
 
         return result
--- blogPosts/index.py
+++ blogPosts/index.py
@@ -1,109 +1,42 @@
-import requests
-from flask import Flask,jsonify,url_for,request,json
+from flask import Flask,request,jsonify,json
 from flask_restful import Api
 from pprint import pprint
-from models.posts import Post,user_posts
+from models.posts import user_posts
 from controllers.getPosts import postRequests
-from marshmallow import Schema,fields
-import json
-
-#myPost = dict(id = 1, author= "Billy Talent", authorid = 2, likes = 123,popularity = .324, reads = 2932,tags = ["health","finance"])
-#myPost = requests.get(url="https://api.hatchways.io/assessment/blog/posts", params={"tag":"tech"}).json()
-
-#this_request = postRequests()
-#my_request = this_request.get("health")[0]["posts"]
-
-"""
-def deserialize(myInput):
-    schema = user_posts(many=True)
-    result = schema.load(myInput)
-    #posts = []
-    #for p in myPost["posts"]:
-    #    result = schema.load(p)
-    #    posts.append(result)
-    return result
-
-def serialize(input_posts):    
-    mySchema = user_posts(many=True)
-    j_result = mySchema.dump(input_posts)
-    return j_result
-
-my_out = deserialize(myPost["posts"])
-print(my_out)
-print("\n\n")
-my_in = serialize(my_out)
-outdict = {"posts":my_in}
-print(outdict)
-"""
-
-#results = []
-"""
-for p in myPost.json():
-    result = schema.load(p)
-    pprint(result)
-    results.append(result)
-"""
-#pprint(result['id'],indent = 2)
-
-#myPost = postRequests()
-#results = myPost.get("health","likes")
-
-#def myFunc(e):
-#    return e['id']
-
-#get_data = requests.get(url="https://api.hatchways.io/assessment/blog/posts", params={"tag":"tech"})
-#newData = get_data.json()
-# newData["posts"].sort(key=myFunc)
-
-#for data in results[0]["posts"]:
-#    print(data)
-
-def deserialize(myInput):
-    schema = user_posts(many=True)
-    result = schema.load(myInput)
-
-    #posts = []
-    #for p in myPost["posts"]:
-    #    result = schema.load(p)
-    #    posts.append(result)
-    return result
-
-def serialize(input_posts):    
-    mySchema = user_posts(many=True)
-    j_result = mySchema.dump(input_posts)
-    return j_result
-
+import asyncio
 
 app = Flask(__name__)
 api = Api(app)
 my_request = postRequests()
 
-#@app.route('ping')
+@app.route('/cache')
+def get_cahce():
+    return jsonify(my_request.cache_storage),200
+
+@app.route('/ping')
+def get_ping():
+    value = {"success":"true"}
+    return jsonify(value)
+
 @app.route('/posts')
 def get_query_string():
     args = request.args
     if(len(args) > 3):
-        return 404
+        return jsonify({"error":"Too many Arguments"}),400
 
     if "tag" in args.keys():
         if "sortBy" in args.keys():
             if"direction" in args.keys():
-                return my_request.get(args["tag"],args["sortBy"],args["direction"])
+                return my_request.get(jsonify(args["tag"],args["sortBy"],args["direction"])),200
             else:
-                return my_request.get(args["tag"],args["sortBy"])
+                return my_request.get(jsonify(args["tag"],args["sortBy"])),200
         else:
-            return my_request.get(args["tag"])
-    else:
-        return 404
-
-    """
-    if(args["tag"] and args["sortBy"]):
-        return my_request.get(str(args["tag"]),str(args["sortBy"]),str(args["direction"]))
-    elif(args["tag"] and args["sortBy"]):
-        return my_request.get(str(args["tag"]),str(args["sortBy"]))
-    elif(args):
-        return my_request.get(str(args["tag"]))
-    """
+            if "direction" in args.keys():
+                return jsonify({"error":"direction should not be used here"})
+            else:
+                return jsonify(my_request.get(args["tag"])),200
+    else: 
+        return jsonify(my_request.tag_error),400
 
 if __name__ == "__main__":
     app.run(host='localhost',port = 5000,debug=True)
